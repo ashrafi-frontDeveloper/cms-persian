aaafa23aac120afbe110b564dccdefd21aab7db1
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-06-29 19:58:11 +0330

    feat(ProductsTable): add product table for /product route

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Errorbox from '../Error/Errorbox'
 import AddNewProduct from '../AddNewProduct/AddNewProduct'
-
+import ProductsTable from '../ProductsTable/ProductsTable'
 export default function Products() {
   return (
     <>
         <Errorbox message="هیچ محصولی یافت نشد" />
 
         <AddNewProduct />
+
+        <ProductsTable />
     </>
   )
 }
